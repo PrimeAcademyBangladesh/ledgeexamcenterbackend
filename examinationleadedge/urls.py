3535dbe743015f30405257cefa0731eceee25c34
Md Hasanuzzaman <dr.has82@gmail.com>
2026-04-30 21:22:42 +0600

    :art: Added all apps to urls

--- examinationleadedge/urls.py
+++ examinationleadedge/urls.py
@@ -2,8 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('apps.users.urls')),
+    path('sessions/', include('apps.sessions.urls')),
+    path('adjustments/', include('apps.adjustments.urls')),
+    path('invigilators/', include('apps.invigilators.urls')),
+    path('learner/', include('apps.learners.urls')),
+    path('results/', include('apps.results.urls')),
+    path('retakes/', include('apps.retakes.urls')),
+    path('questions/', include('apps.questions.urls')),
+    path('reports/', include('apps.reports.urls')),
+    path('qualifications/', include('apps.qualifications.urls')),
+    path('exams/', include('apps.exams.urls')),
+    path('integrity/', include('apps.integrity.urls')),
 ]
