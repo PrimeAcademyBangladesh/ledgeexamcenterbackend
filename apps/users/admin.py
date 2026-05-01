ec1d1e79c7ba62c9cfe0d107822755e0f1ca5a36
Md Hasanuzzaman <dr.has82@gmail.com>
2026-05-01 21:00:32 +0600

    :sparkles: Created django admin user profile

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import User, LearnerProfile, StaffProfile
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "first_name",
+        "last_name",
+        "email",
+        "role",
+        "is_active",
+        "is_staff",
+        "date_joined",
+    )
+    search_fields = ("email", "first_name", "last_name")
+    list_filter = ("role", "is_active", "is_staff")
+
+
+@admin.register(LearnerProfile)
+class LearnerProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "learner_id", "uln", "phone")
+    search_fields = ("user__email", "user__first_name", "user__last_name")
+
+
+@admin.register(StaffProfile)
+class StaffProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "staff_id", "job_title", "staff_id", "phone")
+    search_fields = ("user__email", "user__first_name", "user__last_name")
