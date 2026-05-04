78449a1587280d881037d9e5868529f871391d33
Md Hasanuzzaman <dr.has82@gmail.com>
2026-05-04 21:53:42 +0600

    :art: Change JWT token for chnage 1 minute to test

--- examinationleadedge/settings/base.py
+++ examinationleadedge/settings/base.py
@@ -186,7 +186,7 @@ SIMPLE_JWT = {
     # =====================================================
     # TOKEN LIFETIME
     # =====================================================
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
