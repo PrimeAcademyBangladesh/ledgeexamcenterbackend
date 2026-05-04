4f5f40fa46b1c6321c409434a31dd2c316fdef30
Md Hasanuzzaman <dr.has82@gmail.com>
2026-05-04 22:01:00 +0600

    :art: Change JWT token for change  minute to test

--- examinationleadedge/settings/base.py
+++ examinationleadedge/settings/base.py
@@ -191,6 +191,7 @@ SIMPLE_JWT = {
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": True,
+
     # =====================================================
     # JWT CORE
     # =====================================================
