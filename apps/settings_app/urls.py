2f86ab3145bc2a77f1fbe2844779dccf8c17230f
Md Hasanuzzaman <dr.has82@gmail.com>
2026-05-17 15:49:30 +0600

    :art: Created new brand API

--- apps/settings_app/urls.py
+++ apps/settings_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import SystemSettingsView
+from .views import SystemSettingsView, BrandMiniView
 
 
 urlpatterns = [
     path("", SystemSettingsView.as_view(), name="system-settings"),
+    path("brand-mini/", BrandMiniView.as_view(), name="brand-mini"), 
 ]
